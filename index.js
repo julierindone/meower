9772b2b6d831f88a2c400b82d3d403aa8c180906
Julie Rindone <julie.rindone@gmail.com>
2024-10-02 08:38:16 -0500

    Deleted details from replies

--- index.js
+++ index.js
@@ -146,22 +146,6 @@ function getFeedHtml() {
 							<div class=content>
 								<p class="handle">${reply.handle}</p>
 								<p class="tweet-text">${reply.tweetText}</p>
-								<div class="tweet-details">
-                <span class="tweet-detail">
-									<i class="fa-regular fa-comment-dots" data-reply="${tweet.uuid}"></i>
-									${tweet.replies.length}
-                </span>
-                <span class="tweet-detail">
-									<i class="fa-regular fa-heart ${likeIconClass}" data-like="${tweet.uuid}"></i>
-									${tweet.likes}
-                </span>
-                <span class="tweet-detail">
-									<i class="fa-solid fa-retweet ${retweetIconClass}" data-retweet="${tweet.uuid}"></i>
-									</span>
-                <span class="tweet-detail">
-									<i class="fa-solid fa-trash" id="delete-tweet-${tweet.uuid}" data-delete-tweet="${tweet.uuid}"></i>
-									</span>
-								</div>
 						</div>
 					</div>`
 			})
